cfdaeb1ae377e5558a6a53a3875e28d2710316e4
Timothy Hull <timothyhull@gmail.com>
2020-04-22 13:09:20 -0700

    DNAC show version cURL script

---
+++ sample-files/curl/dnac/04_dnac_sh_ver.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+curl -iX POST \
+-H 'Content-Type: application/json' -H 'X-Auth-Token:'$DNAC_TOKEN \
+--url https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device-poller/cli/read-request \
+--data-raw '{"name":"show ver","commands":["show ver"],"deviceUuids":["3e48558a-237a-4bca-8823-0580b88c6acf"]}'
