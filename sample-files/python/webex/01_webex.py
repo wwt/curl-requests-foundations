c2ef8f5c8f3d8b93ade1819589c37007f12de540
Timothy Hull <timothyhull@gmail.com>
2020-04-24 09:49:36 -0700

    Added Webex Teams cURL & Python scripts

---
+++ sample-files/python/webex/01_webex.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python
+
+# Step 1 - Get a list of your Webex Teams Rooms with the token
+import requests, os
+token = os.getenv('WEBEX_TOKEN')
+headers = {'Content-Type':'application/json','Authorization':f'Bearer {token}'}
+get_url = 'https://api.ciscospark.com/v1/rooms'
+get_response = requests.get(get_url, headers=headers)
+get_response.json() # display all rooms
+get_response.json()['items'][0] # display the first room in the response
+
+# Step 2 - Create a new Webex Teams Room
+new_room = {'title':'New Python Room'}
+post_url = 'https://api.ciscospark.com/v1/rooms'
+post_response = requests.post(post_url, headers=headers, json=new_room)
+print(f'{post_response.status_code} {post_response.reason}')
+print(post_response.json())
+
+# Step 3 - Post a message to the new Webex Teams Room
+new_msg = {'roomId':post_response.json()['id'],'text':'Hello from Python'}
+msg_url = 'https://api.ciscospark.com/v1/messages'
+msg_response = requests.post(msg_url, headers=headers, json=new_msg)
+print(f'{msg_response.status_code} {msg_response.reason}')
+print(msg_response.json())
