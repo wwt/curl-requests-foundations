9d91d69c5babdf0d0e3315776abc09782466d530
Timothy Hull <timothyhull@gmail.com>
2020-05-01 08:53:59 -0700

    Updated XSRF token syntax to match slides

--- sample-files/curl/sdwan/05_sdwan_set_login_banner.sh
+++ sample-files/curl/sdwan/05_sdwan_set_login_banner.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Step 5 - Enable the vManage Login Banner
-curl -X PUT -kib .cookie_jar -H 'X-XSRF-TOKEN: xsrf_token' \
+curl -X PUT -kib .cookie_jar -H 'X-XSRF-TOKEN:'$XSRF_TOKEN \
 -H 'Content-Type: application/json' \
 --data-raw '{mode: "on", bannerDetail: "Welcome to vManage"}' \
 --url https://sandboxsdwan.cisco.com:8443/dataservice/settings/configuration/banner
