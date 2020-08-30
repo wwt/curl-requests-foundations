ef23fa341c824b3ab9a0e332ee5afdaefceb8253
Timothy Hull <timothyhull@gmail.com>
2020-08-30 12:31:59 -0700

    Added -k option to support insecure Cisco DNAC certificate

--- sample-files/curl/dnac/01_dnac_auth.sh
+++ sample-files/curl/dnac/01_dnac_auth.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Step 1 - Authenticate & retrieve a session token
-curl -X POST -iu 'devnetuser:Cisco123!' -H 'Content-Type: application/json' \
+curl -X POST -iku 'devnetuser:Cisco123!' -H 'Content-Type: application/json' \
 --url https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token
--- sample-files/curl/dnac/04_dnac_sh_ver.sh
+++ sample-files/curl/dnac/04_dnac_sh_ver.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Step 4 - Run a 'show version' command on a device
-curl -iX POST \
+curl -ikX POST \
 -H 'Content-Type: application/json' -H 'X-Auth-Token:'$DNAC_TOKEN \
 --url https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device-poller/cli/read-request \
 --data-raw '{"name":"show ver","commands":["show ver"],"deviceUuids":["3e48558a-237a-4bca-8823-0580b88c6acf"]}'
