90331856c7223a597863d33d048864dd79b03dd7
Timothy Hull <timothyhull@gmail.com>
2020-08-30 12:41:48 -0700

    Added code to support insecure Cisco DNAC certificate

--- sample-files/python/dnac/01_dnac.py
+++ sample-files/python/dnac/01_dnac.py
@@ -2,10 +2,11 @@
 
 # Step 1 - Authenticate & retrieve a session token
 import requests
+requests.packages.urllib3.disable_warnings()
 auth_url = 'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token'
 headers = {'Content-Type':'application/json'}
 auth_data = ('devnetuser', 'Cisco123!')
-response = requests.post(auth_url, headers=headers, auth=auth_data)
+response = requests.post(auth_url, headers=headers, auth=auth_data, verify=False)
 print(response.json()) # display the session token
 
 # Step 2 - Store the token in a variable
@@ -16,7 +17,7 @@ print(headers) # display the updated headers
 # Step 3 - Get a list of DNAC devices with the stored token
 from pprint import pprint
 get_url = 'https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device'
-get_response = requests.get(get_url, headers=headers)
+get_response = requests.get(get_url, headers=headers, verify=False)
 pprint(get_response.json()) # display all devices
 print() # blank line between output
 pprint(get_response.json()['response'][0]) #display the first device in the response
@@ -24,6 +25,6 @@ pprint(get_response.json()['response'][0]) #display the first device in the resp
 # Step 4 - Run a 'show version' command on a device
 cli_commands = {'name':'show ver','commands':['show ver'],'deviceUuids':['3e48558a-237a-4bca-8823-0580b88c6acf']}
 post_url = 'https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device-poller/cli/read-request'
-post_response = requests.post(post_url, headers=headers, json=cli_commands)
+post_response = requests.post(post_url, headers=headers, json=cli_commands, verify=False)
 print(f'{post_response.status_code} {post_response.reason}')
 print(post_response.json())
