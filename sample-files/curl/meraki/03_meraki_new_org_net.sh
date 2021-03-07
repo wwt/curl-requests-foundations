b9fb07ed3cc28486f363549170806878621dafd7
Timothy Hull <timothyhull@gmail.com>
2021-03-07 08:40:28 -0800

    Update API version to v1

--- sample-files/curl/meraki/03_meraki_new_org_net.sh
+++ sample-files/curl/meraki/03_meraki_new_org_net.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Step 3 - Create a new Meraki Organization Network
 curl -iLX POST \
---url https://api.meraki.com/api/v0/organizations/549236/networks \
+--url https://api.meraki.com/api/v1/organizations/549236/networks \
 -H 'Content-Type: application/json' -H 'X-Cisco-Meraki-API-Key:'$MERAKI_TOKEN \
 --data-raw '{"name":"LA Office","type": "switch","timeZone":"America/Los_Angeles"}'
--- sample-files/python/meraki/01_meraki.py
+++ sample-files/python/meraki/01_meraki.py
@@ -5,15 +5,15 @@ import requests, os
 from pprint import pprint
 headers = {'Content-Type':'application/json'}
 headers.update({'X-Cisco-Meraki-API-Key':os.getenv('MERAKI_TOKEN')})
-get_url = 'https://api.meraki.com/api/v0/organizations'
+get_url = 'https://api.meraki.com/api/v1/organizations'
 get_response = requests.get(get_url, headers=headers)
 pprint(get_response.json()) # display all organizations
-print() # blank line between output 
+print() # blank line between output
 print(get_response.json()[0]) #display the first organization in the response
 
 # Step 2 - Create a new Meraki network
 new_network = {'name':'LA Office','type': 'switch','timeZone':'America/Los_Angeles'}
-post_url = 'https://api.meraki.com/api/v0/organizations/549236/networks'
+post_url = 'https://api.meraki.com/api/v1/organizations/549236/networks'
 post_response = requests.post(post_url, headers=headers, json=new_network)
 print(f'{post_response.status_code} {post_response.reason}')
 print(post_response.text)
