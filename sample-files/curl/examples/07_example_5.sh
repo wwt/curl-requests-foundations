309bbf9f16ea8d0ea1eb12d066d2da2238f49e1a
Timothy Hull <timothyhull@gmail.com>
2021-03-06 21:33:58 -0800

    Update API version to v1

--- sample-files/curl/examples/07_example_5.sh
+++ sample-files/curl/examples/07_example_5.sh
@@ -2,6 +2,6 @@
 # Create environment variable for Meraki API Token
 export MERAKI_TOKEN=6bec40cf957de430a6f1f2baa056b99a4fac9ea0
 
-curl --url https://api.meraki.com/api/v0/organizations \
+curl --url https://api.meraki.com/api/v1/organizations \
 -H X-Cisco-Meraki-API-Key:$MERAKI_TOKEN \
 -H 'Accept: */*' -i
