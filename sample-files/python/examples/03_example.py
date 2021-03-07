6e964aec4f40bf7ac3ff082d6bf256324007dec7
Timothy Hull <timothyhull@gmail.com>
2021-03-06 21:35:12 -0800

    Remove DNAC insecure SSL code and migrate Meraki API to v1

--- sample-files/python/examples/03_example.py
+++ sample-files/python/examples/03_example.py
@@ -18,16 +18,15 @@ print(response.text)
 
 # Step 3
 import os
-requests.packages.urllib3.disable_warnings()
 req_url = 'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token'
 req_auth = (os.getenv('DNAC_USER'), os.getenv('DNAC_PW'))
-response = requests.post(req_url, auth=req_auth, verify=False)
+response = requests.post(req_url, auth=req_auth)
 print(f'{response.status_code} {response.reason}\n')
 print(f'{response.headers}\n{response.text}')
 
 # Step 4
 from pprint import pprint
-req_url = 'https://api.meraki.com/api/v0/organizations'
+req_url = 'https://api.meraki.com/api/v1/organizations'
 req_headers = {'X-Cisco-Meraki-API-Key':os.getenv('MERAKI_TOKEN')}
 response = requests.get(req_url, headers=req_headers)
 print(f'{response.status_code} {response.reason}\n')
