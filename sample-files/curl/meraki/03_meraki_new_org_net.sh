6bfca35b7d1d68bdbffcabd6a029c82a482cc3a6
Timothy Hull <timothyhull@gmail.com>
2021-03-07 20:08:27 -0800

    Updated body format for API v1

--- sample-files/curl/meraki/03_meraki_new_org_net.sh
+++ sample-files/curl/meraki/03_meraki_new_org_net.sh
@@ -3,4 +3,4 @@
 curl -iLX POST \
 --url https://api.meraki.com/api/v1/organizations/549236/networks \
 -H 'Content-Type: application/json' -H 'X-Cisco-Meraki-API-Key:'$MERAKI_TOKEN \
---data-raw '{"name":"LA Office","type": "switch","timeZone":"America/Los_Angeles"}'
+--data-raw '{"name":"LA Office","productTypes":["switch"],"timeZone":"America/Los_Angeles"}'
--- sample-files/python/meraki/01_meraki.py
+++ sample-files/python/meraki/01_meraki.py
@@ -12,7 +12,7 @@ print() # blank line between output
 print(get_response.json()[0]) #display the first organization in the response
 
 # Step 2 - Create a new Meraki network
-new_network = {'name':'LA Office','type': 'switch','timeZone':'America/Los_Angeles'}
+new_network = {'name':'LA Office','productTypes': ['switch'],'timeZone':'America/Los_Angeles'}
 post_url = 'https://api.meraki.com/api/v1/organizations/549236/networks'
 post_response = requests.post(post_url, headers=headers, json=new_network)
 print(f'{post_response.status_code} {post_response.reason}')
