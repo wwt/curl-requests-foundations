0e364f9877ab604eb848eb77e83edb875fd770e2
Timothy Hull <timothyhull@gmail.com>
2021-03-08 22:32:49 -0800

    Update output file name

--- sample-files/curl/dnac/05_dnac_set_task_id.sh
+++ sample-files/curl/dnac/05_dnac_set_task_id.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Step 5 - If DNAC returns a '202 Accepted', store the task ID as an environment variable
 # Copy the task ID from the response and paste it into the following command, in place of the _______ characters
-# echo export DNAC_TASK=_______ >> 05_dnac_task_id.sh
+# echo export DNAC_TASK=_______ >> 05_dnac_set_task_id.sh
