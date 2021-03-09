e908a2dc1c2721c5d4cac166fafe99f0edd90763
Timothy Hull <timothyhull@gmail.com>
2021-03-08 22:43:02 -0800

    Update output file name

--- sample-files/curl/dnac/07_dnac_set_file_id.sh
+++ sample-files/curl/dnac/07_dnac_set_file_id.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Step 7 - If DNAC returns a file ID,  store it as an environment variable
 # Copy the file ID from the response and paste it into the following command, in place of the _______ characters
-# echo export DNAC_FILE=_______ >> 07_dnac_task_id.sh
+# echo export DNAC_FILE=_______ >> 07_dnac_set_file_id.sh
