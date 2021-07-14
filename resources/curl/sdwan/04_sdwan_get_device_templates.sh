bff534dcc0ebf955020744b2f680324d99169d0f
Timothy Hull <timothyhull@gmail.com>
2021-07-14 08:39:08 +0000

    Update sandbox URL and credentials

--- resources/curl/sdwan/04_sdwan_get_device_templates.sh
+++ resources/curl/sdwan/04_sdwan_get_device_templates.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Step 4 - Get a list of Device Templates
 curl -X GET -kib .cookie_jar -H 'X-XSRF-TOKEN:'$XSRF_TOKEN \
---url https://sandboxsdwan.cisco.com:8443/dataservice/template/device
+--url https://sandbox-sdwan-1.cisco.com/dataservice/template/device
